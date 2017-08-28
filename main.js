ce77d4d1239d94d2e357aba8a11e07d8edf18a1c
Niklas Lindberg <dorkinacage@gmail.com>
2017-08-28 22:01:31 +0200

    First and last commit

---
+++ main.js
@@ -0,0 +1,11 @@
+<input type="text" onkeypress="uniCharCode(event)" onkeydown="uniKeyCode(event)"> 
+
+function uniCharCode(event) {
+    var char = event.which || event.keyCode;
+    document.getElementById("demo").innerHTML = "Unicode CHARACTER code: " + char;
+}
+
+function uniKeyCode(event) {
+    var key = event.keyCode;
+    document.getElementById("demo2").innerHTML = "Unicode KEY code: " + key;
+}
